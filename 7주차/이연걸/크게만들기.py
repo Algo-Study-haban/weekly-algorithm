52a383d62f236ebc3ee06c2ae8a7f3f7c523c25d
YEONGORI <dusrjf14@gmail.com>
2024-03-21 21:17:49 +0900

    [week7] 크게 만들기 문제 풀이

--- 7주차/이연걸/크게만들기.py
+++ 7주차/이연걸/크게만들기.py
@@ -1,3 +1,16 @@
 N, K = map(int, input().split())
-num = int(input())
+num = list(input().rstrip())
+stack = []
 
+for i in range(N):
+    while K > 0 and stack and stack[-1] < num[i]:
+        stack.pop()
+        K -= 1
+    stack.append(num[i])
+
+while K != 0:
+    stack.pop()
+    K -= 1
+
+for s in stack:
+    print(s, end='')
